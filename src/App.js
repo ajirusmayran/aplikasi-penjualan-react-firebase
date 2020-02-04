062b4faf18cbfe442bee23b65639faa5bd8f2c00
ajir <aji.rusmayran@quadras.co.id>
2020-02-04 16:59:51 +0700

    Commit first

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// komponen halaman
+import Registrasi from './pages/registrasi';
+import Login from './pages/login';
+import LupaPassword from './pages/lupa-password';
+import NotFound from './pages/404';
+import Private from './pages/private';
+import PrivateRoute from './components/PrivateRoute';
+
+// firebase context provider
+import FirebaseProvider from './components/FirebaseProvider';
+
+function App() {
+  return (
+
+    <FirebaseProvider>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/" exact component={Private} />
+          <PrivateRoute path="/pengaturan" component={Private} />
+          <PrivateRoute path="/produk" component={Private} />
+          <PrivateRoute path="/transaksi" component={Private} />
+          <Route path="/registrasi" component={Registrasi} />
+          <Route path="/login" component={Login} />
+          <Route path="/lupa-password" component={LupaPassword} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </FirebaseProvider>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBPvu9FmWprY3qDhxRBxIqKNV1vlZbrh0M",
+  authDomain: "aplikasi-penjualan-reactjs-aji.firebaseapp.com",
+  databaseURL: "https://aplikasi-penjualan-reactjs-aji.firebaseio.com",
+  projectId: "aplikasi-penjualan-reactjs-aji",
+  storageBucket: "aplikasi-penjualan-reactjs-aji.appspot.com",
+  messagingSenderId: "932418435552",
+  appId: "1:932418435552:web:9668d35b78371b2f929385",
+  measurementId: "G-RL8BR34N2Q"
+};
+
+export default firebaseConfig;
